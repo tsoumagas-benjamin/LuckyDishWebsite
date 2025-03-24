38ef25201e91221183e2de75db39628c21a5bfc0
tsoumagas-benjamin <bptsoumagas@gmail.com>
2025-03-24 19:20:53 -0400

    Initial commit

---
+++ lucky-dish-website/pages/index.js
@@ -0,0 +1,10 @@
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+    <h1>Lucky Dish Website</h1>
+    <p>Under development</p>
+    </>
+  );
+}
