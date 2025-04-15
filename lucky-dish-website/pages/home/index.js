74c29fb34ff1652f59cb93b3c56e0a553f106be4
unknown <2002zainabaamir@gmail.com>
2025-04-14 20:44:26 -0400

    added statement to homepage

--- lucky-dish-website/pages/home/index.js
+++ lucky-dish-website/pages/home/index.js
@@ -4,16 +4,27 @@ import NavBar from "@/components/navbar.js";
 import PageHeader from "../../components/pageheader.js";
 
 export default function Home() {
-  return (
-    <>
-      <PageHeader />
-      <h2 style={{ textAlign: "center" }}>Welcome to Lucky Dish 🍽️</h2>
-      <nav style={{ display: "flex", justifyContent: "center", gap: "2rem" }}>
-        <Link href="pages/menu"></Link>
-        <Link href="pages/about"></Link>
-        <Link href="pages/login"></Link>
-      </nav>
-      <NavBar />
-    </>
-  );
+    return (
+        <>
+            <PageHeader />
+            <h2 style={{ textAlign: "center" }}>Welcome to Lucky Dish 🍽️</h2>
+            <nav
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    gap: "2rem",
+                }}
+            >
+                <Link href="pages/menu"></Link>
+                <Link href="pages/about"></Link>
+                <Link href="pages/login"></Link>
+            </nav>
+            <NavBar />
+            <p style={{ textAlign: "center" }}>
+                Your go-to spot for comforting, flavorful meals made with love.
+                At Lucky Dish, we serve up delicious dishes inspired by
+                tradition and crafted for today’s taste.
+            </p>
+        </>
+    );
 }
