7d0c936708c3c44f51459e83c91071013bc4cdab
RadHamm <stevenphamm@hotmail.com>
2025-04-20 03:10:33 -0400

    Changed main index.js to redirect to homepage properly, cleaned up code via no code duplication, redundant code amended.

--- lucky-dish-website/pages/index.js
+++ lucky-dish-website/pages/index.js
@@ -1,29 +1,10 @@
-import styles from "@/styles/Home.module.css";
-import About from "./about/index.js";
-import Link from "next/link";
-import NavBar from "@/components/NavBar.js";
-import PageHeader from "../components/pageheader.js";
-import Footer from "@/components/Footer.js";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function Home() {
-    return (
-        <>
-            <PageHeader />
-            <h2 style={{ textAlign: "center" }}>Welcome to Lucky Dish 🍽️</h2>
-            <nav
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    gap: "2rem",
-                }}
-            >
-                <Link href="pages/menu"></Link>
-                <Link href="pages/about"></Link>
-                <Link href="pages/login"></Link>
-            </nav>
-            <NavBar />
-            <br></br>
-            <Footer />
-        </>
-    );
+export default function IndexRedirect() {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/home");
+  }, []);
+  return null;
 }
